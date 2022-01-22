4192f6e229df7f2284ae6c7455a1ecf17e1cb7cf
basacul <antelo.lucas@outlook.com>
2022-01-22 11:59:03 +0000

    Small changes in First server and milestone commit for fortunes_api

--- node_postgres/first_server/index.js
+++ node_postgres/first_server/index.js
@@ -5,7 +5,20 @@ const port = 4500; // for goorm
 const urlInGoorm = 'https://postgresql.run-eu-central1.goorm.io/';
 
 const server = http.createServer((req, res) => {
-	res.end('Welcome to Node!');
+	const { url } = req;
+	
+	console.log(url);
+	
+	if(url === '/translations'){
+		const translations = {1: 'one', 2: 'two', 3: 'three'};
+		res.setHeader('Content-Type', 'application/json');
+		res.write(JSON.stringify(translations));
+		res.end();
+	}else{
+		res.end('Welcome to Node!');
+	}
+	
+	
 });
 
 server.listen(port, hostname, () => {
