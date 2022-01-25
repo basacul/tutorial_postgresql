f91b181029083714fda63fb174eac52ba9be0169
basacul <antelo.lucas@outlook.com>
2022-01-25 20:41:40 +0000

    Implemented REST API /monsters for post request.

--- node_postgres/monsters_api/app.js
+++ node_postgres/monsters_api/app.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const pool = require('./db'); // automatically checks the index.js file
+const bodyParser = require('body-parser');
+const monsters = require('./routes/monsters');
 const app = express();
 
-
-app.get('/monsters', (request,response) => {
-	pool.query('SELECT * FROM monsters ORDER BY id ASC', (err, res) => {
-		if(err){
-			//console.log(err);
-			res.send('There is an internal error and the server crashed...');
-		}
-
-		response.json(res.rows);
-
-	});
+app.use(bodyParser.json());
+app.use('/monsters', monsters);
+app.use((err, req, res, next) => {
+	res.json(err);
 });
 
 module.exports = app;
